e5f6e5263272ec085bff42c7971791099acfdb1f
5000adel <xsrillixon@gmail.com>
2026-05-14 02:51:16 +0800

    feat: added portstemmer for stemming and lemmatization

--- AI.py
+++ AI.py
@@ -1,21 +1,21 @@
 import nltk
 import re
-nltk.download("stopwords")
 
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download("omw-1.4")
 
 text="This handy tool helps you create dummy text for all your layout needs. We are gradually adding new functionality and we welcome your suggestions and feedback."
-
 sentences = sent_tokenize(text)
-# print(sentences[1])
 
-#punctuation remover
-text = re.sub(r"[^a-zA-Z0-9]"," ",sentences[0])
+text = re.sub(r"[^a-zA-Z0-9]"," ",sentences[0]) # Removes Punctuations
 
-words = word_tokenize(text)
-words = [w for w in words if w not in stopwords.words("english")]
+words = word_tokenize(text) # Tokenizes text into words
+words = [w for w in words if w not in stopwords.words("english")] # Removes stopwords
 
 print(words)
