35904240c24df8092373a3307329ef597846c969
5000adel <xsrillixon@gmail.com>
2026-05-14 00:05:35 +0800

    feat: added text punctuation remover with re module, added word tokenizing with word_tokenize from nltk tokenize

--- AI.py
+++ AI.py
@@ -2,10 +2,16 @@ import nltk
 import re
 
 from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 
 text="This handy tool helps you create dummy text for all your layout needs. We are gradually adding new functionality and we welcome your suggestions and feedback."
 
 sentences = sent_tokenize(text)
-print(sentences[1])
+# print(sentences[1])
 
 #punctuation remover
+text = re.sub(r"[^a-zA-Z0-9]"," ",sentences[0])
+
+words = word_tokenize(text)
+
+print(words)
